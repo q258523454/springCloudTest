fd2535309db4d23d07fe671493db19c58987ac4a
zhangj <258523454@qq.com>
2018-11-02 17:42:19 +0800

    spring cloud gateway

---
+++ spring-cloud-gateway/src/main/java/com/springCloudTest/SpringCloudGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.springCloudTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes().route("path_route", r -> r.path("/get").uri("http://httpbin.org")).build();
+    }
+}
